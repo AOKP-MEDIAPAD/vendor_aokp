26ea16f7e54c24601b01df3fbea30bc5c4e11d3a
zyr3x <izzzum@gmail.com>
2012-09-25 13:35:28 +0400

    Update hws7300u product
    Change-Id: I6320b9965fb044b03f9dd4442e59bcbbb283d9e5

--- products/hws7300u.mk
+++ products/hws7300u.mk
@@ -7,6 +7,8 @@ $(call inherit-product, vendor/aokp/configs/gsm.mk)
 # Inherit common product files.
 $(call inherit-product, vendor/aokp/configs/common_tablet_small.mk)
 
+PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/hws7300u
+
 # Setup device specific product configuration.
 PRODUCT_NAME := aokp_hws7300u
 PRODUCT_BRAND := Huawei
@@ -15,4 +17,4 @@ PRODUCT_MODEL := Huawei MediaPad
 PRODUCT_MANUFACTURER : = Huawei
 
 PRODUCT_COPY_FILES += \
-    vendor/aokp/prebuilt/bootanimation/bootanimation_720_1280.zip:system/media/bootanimation.zip
+    vendor/aokp/prebuilt/bootanimation/bootanimation_1280_800.zip:system/media/bootanimation.zip
