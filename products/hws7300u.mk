f9e4c410f88b3de92c110d8ef77c156ec6e9a1d1
zyr3x <izzzum@gmail.com>
2014-03-11 23:47:41 +0300

    Update hws7300u overlays

--- products/hws7300u.mk
+++ products/hws7300u.mk
@@ -7,6 +7,14 @@ $(call inherit-product, vendor/aokp/configs/gsm.mk)
 # Inherit common product files.
 $(call inherit-product, vendor/aokp/configs/common_tablet.mk)
 
+# Grouper Overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/huawei/hws7300u
+
+# boot animation
+PRODUCT_COPY_FILES += \
+    vendor/aokp/prebuilt/bootanimation/bootanimation_720_1280.zip:system/media/bootanimation-alt.zip
+
+
 # Setup device specific product configuration.
 PRODUCT_NAME := aokp_hws7300u
 PRODUCT_BRAND := Huawei
