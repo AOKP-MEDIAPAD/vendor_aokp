e190d08313cc547a8b9a3f68d8595e1e7e5d3321
zyr3x <izzzum@gmail.com>
2014-03-15 12:16:06 +0200

    Update

--- products/hws7300u.mk
+++ products/hws7300u.mk
@@ -1,3 +1,7 @@
+# boot animation
+PRODUCT_COPY_FILES += \
+    vendor/aokp/prebuilt/bootanimation/bootanimation_720_1280.zip:system/media/bootanimation.zip
+
 # Inherit AOSP device configuration for medipad
 $(call inherit-product, device/huawei/hws7300u/device_hws7300u.mk)
 
@@ -9,11 +13,6 @@ $(call inherit-product, vendor/aokp/configs/common_tablet.mk)
 
 # Grouper Overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/huawei/hws7300u
-
-# boot animation
-PRODUCT_COPY_FILES += \
-    vendor/aokp/prebuilt/bootanimation/bootanimation_720_1280.zip:system/media/bootanimation-alt.zip
-
 
 # Setup device specific product configuration.
 PRODUCT_NAME := aokp_hws7300u
